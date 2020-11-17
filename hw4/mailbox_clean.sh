f8e5b5c7c788cf65916f114ee88eb435739321ab
BlankR <hjyblanche@gmail.com>
2020-11-18 02:29:55 +0800

    .

---
+++ hw4/mailbox_clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo deluser inUser
+sudo deluser outUser
+
+sudo rm -rf /home/inUser
+sudo rm -rf /home/outUser
+
+input=("addleness" "analects" "annalistic" "anthropomorphologically" "blepharosphincterectomy" "corector" "durwaun" "dysphasia" "encampment" "endoscopic" "exilic" "forfend" "gorbellied" "gushiness" "muermo" "neckar" "outmate" "outroll" "overrich" "philosophicotheological" "pockwood" "polypose" "refluxed" "reinsure" "repine" "scerne" "starshine" "unauthoritativeness" "unminced" "unrosed" "untranquil" "urushinic" "vegetocarbonaceous" "wamara" "whaledom")
+
+for i in ${input[@]}
+do
+	sudo delgroup $i
+	sudo deluser $i
+done
+
+sudo rm -rf /home/mailbox
